387201279eb308a9a6ee49e2dacb8aea06371dbb
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-04-28 15:07:01 +0100

    Use logger modules more effectively.
    This removes the need for the --print_all_textboxes command line parameter.

--- tools/pdfrename.py
+++ tools/pdfrename.py
@@ -13,8 +13,11 @@ import sys
 import pdfminer.high_level
 import pdfminer.layout
 
+tool_logger = logging.getLogger("pdfrename")
+
 
 def try_santander(text_boxes):
+    logger = tool_logger.getChild("santander")
 
     is_santander_credit_card = any(
         box.get_text() == "Santander Credit Card \n" for box in text_boxes
@@ -32,7 +35,7 @@ def try_santander(text_boxes):
         ]
         assert len(period_line) == 1
 
-        print(repr(period_line[0]))
+        logger.debug("found period specification: %r", period_line[0])
 
         # Very annoying: the date is printed as "18th November 2019" -- which probably
         # would also mean "2nd" if the statement is earlier in the month.
@@ -52,6 +55,8 @@ def try_santander(text_boxes):
 
 
 def try_soenergy(text_boxes):
+    logger = tool_logger.getChild("soenergy")
+
     is_soenergy = any(box.get_text() == "www.so.energy\n" for box in text_boxes)
     if not is_soenergy:
         return None
@@ -59,7 +64,7 @@ def try_soenergy(text_boxes):
     assert text_boxes[1].get_text() == "Hello, here is your statement.\n"
 
     period_line = text_boxes[2].get_text()
-    logging.debug("soenergy found period specification: %s", period_line)
+    logger.debug("found period specification: %r", period_line)
     period_match = re.match(
         r"^For the period of [0-9]{1,2} [A-Z][a-z]{2} [0-9]{4} - ([0-9]{1,2} [A-Z][a-z]{2} [0-9]{4})\n$",
         period_line,
@@ -76,7 +81,7 @@ ALL_FUNCTIONS = (
 )
 
 
-def find_filename(original_filename, print_all_textboxes):
+def find_filename(original_filename):
     first_page = next(pdfminer.high_level.extract_pages(original_filename, maxpages=1))
 
     text_boxes = [
@@ -85,8 +90,7 @@ def find_filename(original_filename, print_all_textboxes):
         if isinstance(obj, pdfminer.layout.LTTextBoxHorizontal)
     ]
 
-    if print_all_textboxes:
-        print(text_boxes)
+    tool_logger.debug("textboxes: %r", text_boxes)
 
     for function in ALL_FUNCTIONS:
         name = function(text_boxes)
@@ -109,11 +113,6 @@ def main():
         ),
     )
     parser.add_argument(
-        "--print_all_textboxes",
-        action="store_true",
-        help="Print all of the textboxes found on the first page, useful for development.",
-    )
-    parser.add_argument(
         "--rename",
         action="store_true",
         help="Whether to actually rename the files, or just output the rename commands.",
@@ -127,13 +126,12 @@ def main():
     )
 
     args = parser.parse_args()
-    logging.basicConfig(level=args.vlog)
+    tool_logger.setLevel(args.vlog)
+    logging.basicConfig()
 
     for original_filename in args.input_files:
         dirname = os.path.dirname(original_filename)
-        new_filename = os.path.join(
-            dirname, find_filename(original_filename, args.print_all_textboxes)
-        )
+        new_filename = os.path.join(dirname, find_filename(original_filename))
         if new_filename == original_filename:
             continue
         if args.rename:
