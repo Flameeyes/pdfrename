6791245d63d747adae1af458721bb4f5e7baec70
Diego Elio Pettenò <flameeyes@flameeyes.com>
2021-12-19 14:15:28 +0000

    Always strip whitespace from account holder name.

--- pdfrename/components.py
+++ pdfrename/components.py
@@ -28,10 +28,13 @@ class NameComponents:
         filename_components.append(self.service_name)
 
         if include_account_holder and self.account_holder:
+            # Always remove stray whitespace, it's never right in this field.
+            account_holder = self.account_holder.strip()
+
             if drop_honorific:
-                filename_components.append(utils.drop_honorific(self.account_holder))
-            else:
-                filename_components.append(self.account_holder)
+                account_holder = utils.drop_honorific(account_holder)
+
+            filename_components.append(account_holder)
 
         filename_components.append(self.document_type)
 
