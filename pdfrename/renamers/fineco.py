2c25bed30f81dc317f9745371557807f6cbd554e
Diego Elio Pettenò <flameeyes@flameeyes.com>
2022-05-04 20:05:01 +0100

    fineco: add profits & losses statement

--- pdfrename/renamers/fineco.py
+++ pdfrename/renamers/fineco.py
@@ -2,12 +2,17 @@
 #
 # SPDX-License-Identifier: MIT
 
+import datetime
+import logging
+import re
 from typing import Optional
 
 import dateparser
 
 from ..lib import pdf_document
 from ..lib.renamer import NameComponents, pdfrenamer
+
+_LOGGER = logging.getLogger(__name__)
 
 
 @pdfrenamer
@@ -37,3 +42,28 @@ def quarterly_statement(
     account_holder = account_holder_str[account_holder_start:].strip()
 
     return NameComponents(date, "FinecoBank", account_holder, "Statement")
+
+
+@pdfrenamer
+def profit_loss(
+    document: pdf_document.Document,
+) -> Optional[NameComponents]:
+    logger = _LOGGER.getChild("profit_loss")
+    try:
+        first_page = document[1]
+    except IndexError:
+        return None
+
+    if not (first_page[0].startswith('P&L SUMMARY\n') and first_page[1].startswith('Dossier: ') and first_page[2].startswith("Dear\n")):
+        return None
+    
+    account_holder = first_page[2].split('\n', 2)[1]
+
+    date_match = re.search(r"realised P&L for transactions settled from \n([0-9]{2}/[0-9]{2}/[0-9]{4})\n", first_page[3])
+    if not date_match:
+        logger.debug("Failed to match date.")
+        return None
+
+    date = datetime.datetime.strptime(date_match.group(1), "%d/%m/%Y")
+
+    return NameComponents(date, "FinecoBank", account_holder, "P&L Summary")
