47e835788d337b1362017b4a54e8e91fd623fa81
Diego Elio Pettenò <flameeyes@flameeyes.com>
2022-05-19 12:31:21 +0100

    o2: update for more recent bills.
    The bill format changed ever so slightly.

--- pdfrename/renamers/o2.py
+++ pdfrename/renamers/o2.py
@@ -2,31 +2,35 @@
 #
 # SPDX-License-Identifier: MIT
 
-from typing import Optional
-
 import dateparser
 
+from ..lib import pdf_document
 from ..lib.renamer import NameComponents, pdfrenamer
 from ..lib.utils import build_dict_from_fake_table, extract_account_holder_from_address
 
 
 @pdfrenamer
-def bill(text_boxes, parent_logger) -> Optional[NameComponents]:
-    logger = parent_logger.getChild("o2.bill")
+def bill(document: pdf_document.Document) -> NameComponents | None:
+    text_boxes = document[1]
 
-    if "Telefónica UK Limited" not in text_boxes[-1]:
+    if "Telefónica UK Limited" not in text_boxes[-1] or text_boxes[0] != "Copy Bill\n":
         return None
 
-    assert text_boxes[0] == "Copy Bill\n"
+    fields_box_index = text_boxes.find_index_starting_with("Account number\n")
 
-    fields_box = text_boxes[1]
-    values_box = text_boxes[2]
-
-    bill_info = build_dict_from_fake_table(fields_box, values_box)
+    bill_info = build_dict_from_fake_table(
+        text_boxes[fields_box_index], text_boxes[fields_box_index + 1]
+    )
     bill_date = dateparser.parse(bill_info["Bill date"], languages=["en"])
     assert bill_date is not None
 
-    address_box = text_boxes[3]
+    # Older bills have the fake table first, followed the address; newer bills use
+    # a more complicated format, but the address is on the second box.
+    if fields_box_index == 1:
+        address_box = text_boxes[3]
+    else:
+        address_box = text_boxes[1]
+
     account_holder_name = extract_account_holder_from_address(address_box)
 
     return NameComponents(
