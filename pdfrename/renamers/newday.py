73936bbb472a137e86edbb33141e93f734615460
Diego Elio Pettenò <flameeyes@flameeyes.com>
2023-04-10 10:40:18 +0100

    newday: relax parser and update to new conventions
    This allows renaming Pulse cards that replaced Amazon's.
    
    It also renames to Credit Card Statement, and include the last four digits of the card like other issuer.

--- pdfrename/renamers/newday.py
+++ pdfrename/renamers/newday.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 import logging
+import re
 
 import dateparser
 
@@ -12,12 +13,17 @@ from ..lib.utils import extract_account_holder_from_address
 
 _LOGGER = logging.getLogger(__name__)
 
+_URLS_TO_ISSUERS = {
+    "\nnewday.co.uk/myamazon\n": "Amazon Platinum MasterCard",
+    "\nnewday.co.uk/mypulse\n": "Pulse",
+}
+
 
 @pdfrenamer
-def amazon_platinum_statement(
+def newday_credit_card_statement(
     document: pdf_document.Document,
 ) -> NameComponents | None:
-    logger = _LOGGER.getChild("amazon_platinum_statement")
+    logger = _LOGGER.getChild("newday_credit_card_statement")
 
     # Try first the first page, but if you see an account number on a short page, take the second.
     text_boxes = document[1]
@@ -33,10 +39,14 @@ def amazon_platinum_statement(
     if not text_boxes:
         return None
 
-    if (
-        "newday.co.uk/myamazon\n" not in text_boxes[0]
-        or "Your Monthly Statement\n" not in text_boxes
-    ):
+    if "Your Monthly Statement\n" not in text_boxes:
+        return None
+
+    for url, issuer in _URLS_TO_ISSUERS.items():
+        if url in text_boxes[0]:
+            break
+    else:
+        logger.debug("Unable to find a valid issue for newday statement.")
         return None
 
     date_box = text_boxes.find_box_starting_with("Statement date: ")
@@ -52,9 +62,25 @@ def amazon_platinum_statement(
 
     account_holder_name = extract_account_holder_from_address(address_box)
 
+    account_number_box = text_boxes.find_box_starting_with("Account number ")
+    if account_number_box:
+        card_number_match = re.match(
+            "^Account number [0-9]{12}([0-9]{4})\n$", account_number_box
+        )
+    else:
+        account_number_box_index = text_boxes.index("Account number\n")
+        if account_number_box_index:
+            card_number_box = text_boxes[account_number_box_index + 1]
+            card_number_match = re.match("^[0-9]{12}([0-9]{4})\n$", card_number_box)
+
+    additional_components = []
+    if card_number_match:
+        additional_components.append(f"xx-{card_number_match.group(1)}")
+
     return NameComponents(
         statement_date,
-        "Amazon Platinum MasterCard",
+        issuer,
         account_holder_name,
-        "Statement",
+        "Credit Card Statement",
+        additional_components=additional_components,
     )
