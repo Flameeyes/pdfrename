651b748429b280f51ea26cb780e447d8dc3564b5
Diego Elio Pettenò <flameeyes@flameeyes.com>
2023-11-26 21:37:39 +0000

    o2: add support for non-copy bills.
    O2 now has a different template for the last two bills downloaded from
    their website, which requires a similar, but different, implementation.

--- pdfrename/renamers/o2.py
+++ pdfrename/renamers/o2.py
@@ -2,15 +2,24 @@
 #
 # SPDX-License-Identifier: MIT
 
+import logging
+
 import dateparser
 
 from ..lib import pdf_document
 from ..lib.renamer import NameComponents, pdfrenamer
 from ..lib.utils import build_dict_from_fake_table, extract_account_holder_from_address
 
+_LOGGER = logging.getLogger(__name__)
+
 
 @pdfrenamer
-def bill(document: pdf_document.Document) -> NameComponents | None:
+def uk_copy_bill(document: pdf_document.Document) -> NameComponents | None:
+    """Parse and rename copy bills from My O2 (UK) service.
+
+    This applies to the archive of bills, rather than the original "My Bill"
+    provided in the main landing page of billings.
+    """
     text_boxes = document[1]
 
     if (
@@ -44,3 +53,36 @@ def bill(document: pdf_document.Document) -> NameComponents | None:
         account_holder_name,
         "Bill",
     )
+
+
+@pdfrenamer
+def uk_original_bill(document: pdf_document.Document) -> NameComponents | None:
+    logger = _LOGGER.getChild("uk_original_bill")
+
+    text_boxes = document[1]
+
+    if len(text_boxes) < 5:
+        return None
+
+    if not text_boxes[1].startswith("O2.co.uk/help | "):
+        return None
+
+    logger.debug(
+        "Found likely O2 bill.\n  Bill info keys: %r\n  Bill info values: %r",
+        text_boxes[3],
+        text_boxes[4],
+    )
+
+    bill_info = build_dict_from_fake_table(text_boxes[3], text_boxes[4])
+
+    if (account_holder_name := bill_info.get("Account name")) is None:
+        logging.warning("Unable to find account holder name.")
+        return None
+
+    if (bill_date_str := bill_info.get("Bill date")) is None:
+        logging.warning("Unable to find bill date.")
+        return None
+
+    bill_date = dateparser.parse(bill_date_str, languages=["en"])
+
+    return NameComponents(bill_date, "O2 UK", account_holder_name, "Bill")
