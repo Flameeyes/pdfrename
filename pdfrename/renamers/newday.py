754fcf2c451a995fff98861b3d74a28e5dd002fb
Diego Elio Pettenò <flameeyes@flameeyes.com>
2024-04-13 17:08:11 +0100

    newday: support the new 2024 format.
    This required some minimal changes from the previous one.

--- pdfrename/renamers/newday.py
+++ pdfrename/renamers/newday.py
@@ -13,10 +13,14 @@ from ..lib.utils import extract_account_holder_from_address
 
 _LOGGER = logging.getLogger(__name__)
 
-_URLS_TO_ISSUERS = {
-    "\nnewday.co.uk/myamazon\n": "Amazon Platinum MasterCard",
-    "\nnewday.co.uk/mypulse\n": "Pulse",
-}
+_URLS_TO_ISSUERS = (
+    (re.compile(r"\bnewday.co.uk/myamazon\n"), "Amazon Platinum MasterCard"),
+    (re.compile(r"\bnewday.co.uk/mypulse\n"), "Pulse"),
+)
+
+# 17 characters: 16-digit PAN of the card (Account Number) plus newline.
+# 736501 is the BIN for NewDay.
+_NEWDAY_BIN_PATTERN = re.compile(r"^736501\d{10}\n$")
 
 
 @pdfrenamer
@@ -28,25 +32,42 @@ def newday_credit_card_statement(
     # Try first the first page, but if you see an account number on a short page, take the second.
     text_boxes = document[1]
     if len(text_boxes) == 2:
-        # 17 characters: 16-digit PAN of the card (Account Number) plus newline.
-        # 736501 is the BIN for NewDay/Amazon Platinum.
-        if len(text_boxes[0]) == 17 and text_boxes[0].startswith("736501"):
+        if _NEWDAY_BIN_PATTERN.match(text_boxes[0]):
             logger.debug(
-                f"{document.original_filename}: possible NewDay statement, looking at the second page."
+                "%s: possible NewDay statement, looking at the second page.",
+                document.original_filename,
             )
             text_boxes = document[2]
+    elif not set(text_boxes.find_all_matching_regex(_NEWDAY_BIN_PATTERN)):
+        return None
 
     if not text_boxes:
         return None
 
-    if "Your Monthly Statement\n" not in text_boxes:
+    logger.debug("%s: possible NewDay statement.", document.original_filename)
+
+    if (
+        "Your Monthly Statement\n" not in text_boxes
+        and not text_boxes.find_box_starting_with("Your monthly statement\n")
+    ):
         return None
 
-    for url, issuer in _URLS_TO_ISSUERS.items():
-        if url in text_boxes[0]:
+    if "newday.co.uk/" in text_boxes[0]:
+        manage_link_box = text_boxes[0]
+    elif "newday.co.uk/" in text_boxes[-1]:
+        manage_link_box = text_boxes[-1]
+    else:
+        logger.debug("%s: unable to find management link.", document.original_filename)
+        return None
+
+    for url, issuer in _URLS_TO_ISSUERS:
+        if url.search(manage_link_box):
             break
     else:
-        logger.debug("Unable to find a valid issue for newday statement.")
+        logger.debug(
+            "%s: unable to find a valid issuer for newday statement.",
+            document.original_filename,
+        )
         return None
 
     date_box = text_boxes.find_box_starting_with("Statement date: ")
