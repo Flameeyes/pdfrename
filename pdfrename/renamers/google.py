fe00ef6fe64c8a5c37b18a89181329b14790fa16
Diego Elio Pettenò <flameeyes@flameeyes.com>
2025-10-31 16:46:16 +0000

    google: add invoice number to the metadata

--- pdfrename/renamers/google.py
+++ pdfrename/renamers/google.py
@@ -21,17 +21,31 @@ def invoice(text_boxes, parent_logger) -> NameComponents | None:
     address_idx = text_boxes.index("Bill to\n")
     account_holder_name = text_boxes[address_idx + 1].strip()
 
-    invoice_date_title_box = find_box_starting_with(text_boxes, "Invoice date\n")
-    assert invoice_date_title_box
+    invoice_number_box = find_box_starting_with(text_boxes, "Invoice number\n")
+    assert invoice_number_box
+    match invoice_number_box.count("\n"):
+        case 2:
+            invoice_number_idx = text_boxes.index(invoice_number_box) + 1
+            invoice_number = text_boxes[invoice_number_idx].strip()
+        case 3:
+            invoice_number = invoice_number_box.split("\n")[2]
 
-    invoice_date_box = invoice_date_title_box.split("\n")
-    if len(invoice_date_box) == 3:
-        invoice_date_idx = text_boxes.index(invoice_date_title_box) + 1
-        invoice_date_str = text_boxes[invoice_date_idx]
-    elif len(invoice_date_box) == 4:
-        invoice_date_str = invoice_date_box[2]
+    invoice_date_box = find_box_starting_with(text_boxes, "Invoice date\n")
+    assert invoice_date_box
+    match invoice_date_box.count("\n"):
+        case 2:
+            invoice_date_idx = text_boxes.index(invoice_date_box) + 1
+            invoice_date_str = text_boxes[invoice_date_idx]
+        case 3:
+            invoice_date_str = invoice_date_box.split("\n")[2]
 
     invoice_date = dateparser.parse(invoice_date_str, languages=["en"])
     assert invoice_date is not None
 
-    return NameComponents(invoice_date, "Google", account_holder_name, "Invoice")
+    return NameComponents(
+        invoice_date,
+        "Google",
+        account_holder_name,
+        "Invoice",
+        document_number=invoice_number,
+    )
